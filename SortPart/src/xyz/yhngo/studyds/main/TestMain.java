43795363726007d827b6695dc387d9ff68f87d63
延昊南 <yhnkb@hotmail.com>
2016-07-21 16:42:19 +0800

    ready for sort module

---
+++ SortPart/src/xyz/yhngo/studyds/main/TestMain.java
@@ -0,0 +1,12 @@
+package xyz.yhngo.studyds.main;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by Vegeta on 7/21/16.
+ */
+public class TestMain {
+    public static void main(String args[]) {
+        StdOut.println("hi");
+    }
+}
