c291cacd0cb9839fb0ec275e1fed9a8b824c77a4
延昊南 <yhnkb@hotmail.com>
2016-07-23 15:56:33 +0800

    查找部分:基于无序链表的顺序查找、基于有序数组的二分查找

---
+++ SearchPart/src/xyz/yhngo/studyds/main/FrequencyConter.java
@@ -0,0 +1,31 @@
+package xyz.yhngo.studyds.main;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by Vegeta on 7/23/16.
+ */
+public class FrequencyConter {
+    public static void main(String args[]) {
+        int minlen = Integer.parseInt(args[0]);
+        SequentialSearchST<String, Integer> st = new SequentialSearchST<String, Integer>();
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (word.length() < minlen)
+                continue;
+            if (!st.contains(word))
+                st.put(word, 1);
+            else
+                st.put(word, st.get(word) + 1);
+        }
+
+        String max = " ";
+        st.put(max, 0);
+        for (String word : st.keys())
+            if (st.get(word) > st.get(max))
+                max = word;
+
+        StdOut.println(max + " " + st.get(max));
+    }
+}
