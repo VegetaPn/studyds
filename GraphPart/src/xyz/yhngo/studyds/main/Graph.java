1e2a91d5bdff2a4f24c0a02ca1cfb8b7d1a9064c
延昊南 <yhnkb@hotmail.com>
2016-07-25 15:37:32 +0800

    Graph

---
+++ GraphPart/src/xyz/yhngo/studyds/main/Graph.java
@@ -0,0 +1,48 @@
+package xyz.yhngo.studyds.main;
+
+import edu.princeton.cs.algs4.In;
+
+/**
+ * Created by Vegeta on 7/25/16.
+ */
+public class Graph {
+    private final int V;    // 定点数目
+    private int E;  // 边的数目
+    private Bag<Integer>[] adj; // 邻接表
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<Integer>();
+    }
+
+    public Graph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+}
