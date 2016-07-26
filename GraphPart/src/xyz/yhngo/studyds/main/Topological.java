241cd212c3f40c7025eae0d3b124f82c3acebf1e
延昊南 <yhnkb@hotmail.com>
2016-07-26 12:01:03 +0800

    有向图中基于深度优先搜索的顶点排序、拓扑排序

---
+++ GraphPart/src/xyz/yhngo/studyds/main/Topological.java
@@ -0,0 +1,39 @@
+package xyz.yhngo.studyds.main;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.SymbolDigraph;
+
+/**
+ * Created by Vegeta on 7/26/16.
+ */
+public class Topological {
+    private Iterable<Integer> order;
+
+    public Topological(Digraph G) {
+        DirectedCycle cyclefinder = new DirectedCycle(G);
+        if (!cyclefinder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
+        }
+    }
+
+    public Iterable<Integer> order() {
+        return order;
+    }
+
+    public boolean isDAG() {
+        return order != null;
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+        String separator = args[1];
+        SymbolDigraph sg = new SymbolDigraph(filename, separator);
+
+//        Topological top = new Topological(sg.G());
+//
+//        for (int v : top.order()) {
+//            StdOut.println(sg.name(v));
+//        }
+    }
+}
