8dcd651d627b3b54121344454b5b143996c48ca3
延昊南 <yhnkb@hotmail.com>
2016-07-26 16:19:16 +0800

    最小生成树的Prim算法的延时实现

---
+++ GraphPart/src/xyz/yhngo/studyds/main/LazyPrimMST.java
@@ -0,0 +1,45 @@
+package xyz.yhngo.studyds.main;
+
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+
+/**
+ * Created by Vegeta on 7/26/16.
+ */
+public class LazyPrimMST {
+    private boolean[] marked;
+    private Queue<Edge> mst;
+    private MinPQ<Edge> pq;
+
+    public LazyPrimMST(EdgeWeightedGraph G) {
+        pq = new MinPQ<Edge>();
+        marked = new boolean[G.V()];
+        mst = new Queue<Edge>();
+
+        visit(G, 0);
+        while (!pq.isEmpty()) {
+            Edge e = pq.delMin();
+
+            int v = e.either(), w = e.other(v);
+            if (marked[v] && marked[w])
+                continue;
+            mst.enqueue(e);
+            if (!marked[v])
+                visit(G, v);
+            if (!marked[w])
+                visit(G, w);
+        }
+    }
+
+    private void visit(EdgeWeightedGraph G, int v) {
+        marked[v] = true;
+        for (Edge e : G.adj(v)) {
+            if (!marked[e.other(v)])
+                pq.insert(e);
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+}
