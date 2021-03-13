7ba63efd7b2b9b1e165c60d6291dc5128c7e4be2
VegetaPn <yhnkb@hotmail.com>
2021-03-13 19:00:54 +0800

    union find

---
+++ 2021/src/main/java/me/yanhaonan/uf/UF.java
@@ -0,0 +1,52 @@
+package me.yanhaonan.uf;
+
+public class UF {
+    private int count;
+    private int[] parent;
+    private int[] size;
+
+    public UF(int n) {
+        this.count = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if (rootP == rootQ) {
+            return;
+        }
+
+        if (size[rootP] > size[rootQ]) {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        } else {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        count--;
+    }
+
+    public boolean isConnect(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+
+    public int count() {
+        return this.count;
+    }
+}
